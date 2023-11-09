c6c66aa50fbc7a7559a02f912f54cf961889f001
BrewMan29 <karma95@gmail.com>
2023-11-09 23:58:47 +0900

    3장. WebDownloader 예제

---
+++ CS_Practice/WebDownloader/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WebDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.cbOpen = new System.Windows.Forms.CheckBox();
+            this.pgbDownload = new System.Windows.Forms.ProgressBar();
+            this.webClient = new System.Net.WebClient();
+            this.fbdFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lbUrl
+            // 
+            this.lbUrl.AutoSize = true;
+            this.lbUrl.Location = new System.Drawing.Point(37, 29);
+            this.lbUrl.Name = "lbUrl";
+            this.lbUrl.Size = new System.Drawing.Size(44, 18);
+            this.lbUrl.TabIndex = 0;
+            this.lbUrl.Text = "주소";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(87, 26);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(591, 28);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Enabled = false;
+            this.btnDown.Location = new System.Drawing.Point(156, 88);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(148, 54);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "다운로드";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(340, 88);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(148, 54);
+            this.btnFolder.TabIndex = 2;
+            this.btnFolder.Text = "폴더";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // cbOpen
+            // 
+            this.cbOpen.AutoSize = true;
+            this.cbOpen.Location = new System.Drawing.Point(523, 105);
+            this.cbOpen.Name = "cbOpen";
+            this.cbOpen.Size = new System.Drawing.Size(88, 22);
+            this.cbOpen.TabIndex = 3;
+            this.cbOpen.Text = "창열기";
+            this.cbOpen.UseVisualStyleBackColor = true;
+            // 
+            // pgbDownload
+            // 
+            this.pgbDownload.Location = new System.Drawing.Point(87, 183);
+            this.pgbDownload.Name = "pgbDownload";
+            this.pgbDownload.Size = new System.Drawing.Size(591, 47);
+            this.pgbDownload.TabIndex = 4;
+            // 
+            // webClient
+            // 
+            //this.webClient.AllowReadStreamBuffering = true;
+            //this.webClient.AllowWriteStreamBuffering = true;
+            this.webClient.BaseAddress = "";
+            this.webClient.CachePolicy = null;
+            this.webClient.Credentials = null;
+            this.webClient.Encoding = ((System.Text.Encoding)(resources.GetObject("webClient.Encoding")));
+            this.webClient.Headers = ((System.Net.WebHeaderCollection)(resources.GetObject("webClient.Headers")));
+            this.webClient.QueryString = ((System.Collections.Specialized.NameValueCollection)(resources.GetObject("webClient.QueryString")));
+            this.webClient.UseDefaultCredentials = false;
+            this.webClient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.webClient_DownloadFileCompleted);
+            this.webClient.DownloadProgressChanged += new System.Net.DownloadProgressChangedEventHandler(this.webClient_DownloadProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pgbDownload);
+            this.Controls.Add(this.cbOpen);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lbUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "웹 다운로드";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUrl;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.CheckBox cbOpen;
+        private System.Windows.Forms.ProgressBar pgbDownload;
+        private System.Net.WebClient webClient;
+        private System.Windows.Forms.FolderBrowserDialog fbdFile;
+    }
+}
+
---
+++ CS_Practice/WebDownloader/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WebDownloader
+{
+    public partial class Form1 : Form
+    {
+
+
+        Boolean isBusy;     //progressbar 상태를 나타냄
+        private string filePath = null; //파일 경로
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            if(this.fbdFile.ShowDialog() == DialogResult.OK)
+            {
+                this.btnDown.Enabled = true;
+                filePath = this.fbdFile.SelectedPath;
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (isBusy)
+            {
+                webClient.CancelAsync();
+                isBusy = false;
+            }
+            else
+            {
+                try
+                {
+                    var strFileName = this.txtUrl.Text.Split(new Char[] { '/' });
+                    System.Array.Reverse(strFileName);
+                    Uri uri = new Uri(this.txtUrl.Text);
+
+                    //파일의 유효성 검사
+                    var str = webClient.DownloadString(uri);
+                    webClient.DownloadFileAsync(uri, filePath + @"\" + strFileName[0]);
+                    isBusy = true;
+                }
+                catch
+                {
+                    this.btnDown.Enabled=false;
+                    MessageBox.Show("다운로드에 실퍠하였습니다.", "에러",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void webClient_DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
+            this.pgbDownload.Value = e.ProgressPercentage;
+
+        }
+
+        private void webClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            isBusy = false;
+            this.btnDown.Enabled = false;
+            if(e.Error == null)
+            {
+                if(this.cbOpen.Checked == false)
+                {
+                    MessageBox.Show("다운로드가 완료되었습니다.", "알림",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Process myprocess = new Process();
+                    myprocess.StartInfo.FileName = filePath;
+                    myprocess.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("다운로드가 실패하였습니다. : " + e.Error.Message.ToString());
+            }
+
+        }
+    }
+}
